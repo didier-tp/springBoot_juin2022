bedd1492e40e92d17cddf806b8a164778b668d5b
DidierDefrance <didier@d-defrance.fr>
2022-06-27 18:00:38 +0200

    sans partie mvnw quelquefois genante pour intellij

--- tp/appliSpring/src/main/java/tp/appliSpring/entity/Compte.java
+++ tp/appliSpring/src/main/java/tp/appliSpring/entity/Compte.java
@@ -36,28 +36,32 @@ public class Compte {
 		super();
 	}
 	
-	
-	public Long getNumero() {
-		return numero;
-	}
-	public void setNumero(Long numero) {
-		this.numero = numero;
-	}
-	public String getLabel() {
-		return label;
-	}
-	public void setLabel(String label) {
-		this.label = label;
-	}
-	public Double getSolde() {
-		return solde;
-	}
-	public void setSolde(Double solde) {
-		this.solde = solde;
-	}
+
 
     //+get/set , constructeur , toString()
 
-	
-	
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(Double solde) {
+		this.solde = solde;
+	}
 }
