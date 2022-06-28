f6b89032e0837ebe343c483c1de4d7568c36740e
DidierDefrance <didier@d-defrance.fr>
2022-06-28 10:19:06 +0200

    aspect

--- tp/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
+++ tp/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
@@ -2,8 +2,10 @@ package tp.appliSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class AppliSpringApplication {
 
 	public static void main(String[] args) {
