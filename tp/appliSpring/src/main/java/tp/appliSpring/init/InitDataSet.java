dee35e1b8b5380674591e3e1cac94a733cf6c661
DidierDefrance <didier@d-defrance.fr>
2022-06-29 10:54:14 +0200

    client 1 et 2

--- tp/appliSpring/src/main/java/tp/appliSpring/init/InitDataSet.java
+++ tp/appliSpring/src/main/java/tp/appliSpring/init/InitDataSet.java
@@ -27,12 +27,17 @@ public class InitDataSet {
 		Client client1 = serviceClient.sauvegarderClient(new Client(null,"jean","Bon"));
 		Compte cptA = new Compte(null,"compteA1",101.0);
 		cptA.setClient(client1);
-		Compte cptA_sauvegarde = serviceCompte.sauvegarderCompte(cptA);
+		serviceCompte.sauvegarderCompte(cptA);
 		Compte cptB = new Compte(null,"compteB1",51.0);
 		cptB.setClient(client1);
-		Compte cptB_sauvegarde = serviceCompte.sauvegarderCompte(cptB);
+		serviceCompte.sauvegarderCompte(cptB);
 		
-		serviceCompte.sauvegarderCompte(new Compte(null,"compteC",80.0));
-		serviceCompte.sauvegarderCompte(new Compte(null,"compteD",20.0));
+		Client client2 = serviceClient.sauvegarderClient(new Client(null,"alex","Therieur"));
+		Compte cptC = new Compte(null,"compteC",80.0);
+		cptC.setClient(client2);
+		serviceCompte.sauvegarderCompte(cptC);
+		Compte cptD = new Compte(null,"compteD",20.0);
+		cptD.setClient(client2);
+		serviceCompte.sauvegarderCompte(cptD);
 	}
 }
